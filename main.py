dc1f314620dc4be2257aa710fc09fc29c3eb310b
Vasileios Karavasilis <cyane.spark@gmail.com>
2022-12-09 00:38:35 +0200

    Separated features after breaking data into healthy and sick lists

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ from naive_bayes import *
 data = pre_process()
 training_data, testing_data = k_fold_cross_validation(5,data)
 
-features = []
-
-#transposing the training_data will create a seperate list for each feature
-for data in training_data:
-    features.append(list(map(list, zip(*data))))
-
-train_naive_bayes(features[0])
+train_naive_bayes(training_data[0])
--- naive_bayes.py
+++ naive_bayes.py
@@ -2,29 +2,42 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy.stats as stats
 
-def train_naive_bayes(features_data):
+def train_naive_bayes(training_data):
+
+    sick_data = [data for data in training_data if data[10]==2.0]
+    healthy_data = [data for data in training_data if data[10]==1.0]
     
-    mean = [0]* len(features_data)
-    std = [0]* len(features_data)
-    curves = [0]* len(features_data)
-    for i in range(len(features_data)):
-        mean[i]= np.mean(features_data[i])
-        std[i] = np.std(features_data[i])
-        curves[i] = stats.norm(mean[i], std[i])
-    #plt.plot(curves[0])
-    #count, bins, ignored = plt.hist(curves[0], 30, density=True)
-        #plt.plot(bins, 1/(std[i]*np.sqrt(2*np.pi))*np.exp(-(bins-mean[i])**2/(2*std[i]**2)), linewidth=2, color='r')
+    print(sick_data[0])
+    print(healthy_data[0])
 
-    print(mean[0])
-    print(std[0])
-    print(features_data[0])
 
-    x = np.linspace(mean[0]-4*std[0],mean[0]+4*std[0],100)
-    plt.plot(x, curves[0].pdf(x), linewidth=2, color='r')
-    #plt.hist(features_data[0],100)
-    plt.axis([mean[0]-4*std[0],mean[0]+4*std[0], 0, 1.5])
-    plt.scatter(features_data[0],467*[0], color='k', )
-    plt.show()
+    sick_data_features = seperate_features(sick_data)
+    healthy_data_features = seperate_features(healthy_data)
+
+    print(sick_data_features[1])
+    print(healthy_data_features[1])
+
+    #mean = [0]* len(features_data)
+    #std = [0]* len(features_data)
+    #pdfs = [0]* len(features_data)
+    
+    #for i in range(len(features_data)):
+     #   mean[i]= np.mean(features_data[i])
+      #  std[i] = np.std(features_data[i])
+       # pdfs[i] = stats.norm(mean[i], std[i])
+
+    
+    #x = np.linspace(mean[0]-4*std[0],mean[0]+4*std[0],100)
+    #plt.plot(x, pdfs[0].pdf(x), linewidth=2, color='r')
+    #plt.axis([mean[0]-4*std[0],mean[0]+4*std[0], 0, 1.5])
+    #plt.scatter(features_data[0],len(features_data[0])*[0], color='k')
+    #plt.show()
 
     return 5
-        
+
+#transposing the training_data will create a seperate list for each feature    
+def seperate_features(data):
+    features = list(map(list, zip(*data)))
+
+    del(features[-1])
+    return features
