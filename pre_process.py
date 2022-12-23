0a640f49ab3db5cd0bebfc24b61777fa678f8f4f
Vasileios Karavasilis <cyane.spark@gmail.com>
2022-12-23 17:44:11 +0200

    Final version of files

--- pre_process.py
+++ pre_process.py
@@ -8,35 +8,44 @@ def pre_process(filename='Indian Liver Patient Dataset (ILPD).csv'):
         csv_reader = csv.reader(file, delimiter=',')
         for row in csv_reader:
             data.append(row);
-
     file.close()
 
-    categories = len(data[0])
+    features = len(data[0])
+    data_min = [1000]*features
+    data_max = [-1]*features
+    total = [0]*features
+    count = [0]*features
     
-    data_min = [1000]*categories
-    data_max = [-1]*categories
-
     for entry in data:
         if entry[1]=='Female':
             entry[1] = 1
         else:
             if entry[1]=='Male':
                 entry[1] = 0
-        for i in range(categories):
+        for i in range(features):
             if entry[i]=='':
-                entry[i] = 0
+                continue;
+                
             entry[i] = float(entry[i])
+            total[i] += entry[i]
+            count[i] += 1
+    
             if data_min[i] > entry[i]:
                 data_min[i] = entry[i]
             if data_max[i] < entry[i]:
                 data_max[i] = entry[i]
-
+                    
+    for entry in data:
+        for i in range(features):
+            if entry[i]=='':
+                entry[i]=1.0*total[i]/count[i]
+                
     data_range=[]
-    for i in range(categories):
+    
+    for i in range(features):
         data_range.append(data_max[i]-data_min[i])  
 
     for entry in data:
-        for i in range(categories-1):
+        for i in range(features-1):
             entry[i] = -1 +(((entry[i]-data_min[i])*(2))/data_range[i])
-
-    return data;      
+    return data
